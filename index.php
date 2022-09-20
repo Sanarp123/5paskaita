6fd17800f127554247f56beac70eecb33d1c7289
Sanarp123 <105873022+Sanarp123@users.noreply.github.com>
2022-09-20 19:17:17 +0300

    ciklas pridetas vardo generavimui

--- index.php
+++ index.php
@@ -26,7 +26,35 @@ if(isset($_POST["upload"])){
     // var_dump($file);
     $file_dir = "uploads/";
     //nurodtyi kur bus failas ir kaip vadinsis
-    $file_path = $file_dir . $file["name"];
+
+    // 1.random string failu vardui
+    // 2.Kokie ikelti
+    // 3.sha256 kodavimas sugeneravimas. Unikalus, taciau yra minimali tikimybe, kad uzkoduos vienodai
+    // 4.base64 kodavimas
+
+    $file_name_generated = "failas.jpg";
+
+    // 1. koks yra failas (extension).
+    $file_name_array = explode(".",$file["name"]);
+    $file_extension = $file_name_array[1];
+    // 2. Atsitiktinio stringo.
+    $random_words = ["a","b","c","d","e"];
+    // 3. sugeneruoti pavadinima
+    
+    $random_string = "";
+    for($i = 0; $i <=22; $i++) {
+        $random_number = rand(0,count($random_words)-1); // 0, 1, 2, 3, 4
+        // $random_string = $random_string . $random_words[$random_number];
+        $random_string .= $random_words[$random_number];
+    }
+
+
+    $file_name_generated = $random_string .".".$file_extension;
+
+
+    var_dump($file_name_generated); 
+
+     $file_path = $file_dir . $file_name_generated;
     // leisti tik tam tikra tipa, dydi ir tt
     // move_uploaded_file - jei ikelimas sekmingas - true ir ikelia  (perkelia is temp folderio) faila.
     if(move_uploaded_file($file["tmp_name"],$file_path)) {
